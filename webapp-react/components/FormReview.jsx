99145b4229d2f271dbf392cf3d9069cf5cef430e
FrancescoMarra97 <francescomarra192@gmail.com>
2024-12-13 16:49:42 +0100

    last commit

--- webapp-react/components/FormReview.jsx
+++ webapp-react/components/FormReview.jsx
@@ -1,7 +1,41 @@
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function FormReview() {
+    const { id } = useParams()
+    const [name, setName] = useState('');
+    const [vote, setVote] = useState('');
+    const [text, SetText] = useState('');
+
     function HandleFormSubmit(e) {
-        console.log(e);
-        e.preventDefault()
+        //e.preventDefault()
+
+        const url = `http://localhost:3001/movies/${id}/review`
+
+
+        const formData = {
+            name,
+            vote,
+            text
+        }
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+
+            })
+            .catch(err => console.log(err))
+
+        setName("")
+        setVote(0)
+        SetText("")
     }
 
     return (
@@ -13,16 +47,16 @@ export default function FormReview() {
                             <div>
                                 <label htmlFor="name">il tuo nickname</label>
                             </div>
-                            <input name="name" type="text" placeholder="inserisci il tuo nome" />
+                            <input name="name" type="text" className="form-control" placeholder="inserisci il tuo nome" value={name} onChange={(e) => setName(e.target.value)} required />
                         </div>
                         <div className="mb-3">
-                            {/* ratings */}
+                            {[1, 2, 3, 4, 5].map(n => <i key={n} className={`bi bi-star${n <= vote ? '-fill' : ''} `} onClick={() => setVote(n)}></i>)}
                         </div>
                         <div className="mb-3">
                             <div>
-                                <label htmlFor="review">la tua recensione</label>
+                                <label htmlFor="text">la tua recensione</label>
                             </div>
-                            <textarea name="review" id="review" placeholder="lascia la tua recensione" ></textarea>
+                            <textarea name="text" id="text" className="form-control" placeholder="lascia la tua recensione" value={text} onChange={(e) => SetText(e.currentTarget.value)} required ></textarea>
                         </div>
                         <div>
                             <button type="submit" className="btn btn-dark">Invia</button>
