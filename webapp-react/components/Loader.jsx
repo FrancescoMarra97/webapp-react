07fe60ceb51523036b0fa79627f70608b0d7b034
FrancescoMarra97 <francescomarra192@gmail.com>
2024-12-16 12:49:37 +0100

    loader

---
+++ webapp-react/components/Loader.jsx
@@ -0,0 +1,17 @@
+import 'ldrs/lineSpinner'
+export default function Loader() {
+
+    return (
+
+
+        // Default values shown  
+        <l-line-spinner
+            size="40"
+            stroke="3"
+            speed="1"
+            color="black"
+        ></l-line-spinner>
+
+
+    )
+}
--- webapp-react/src/App.jsx
+++ webapp-react/src/App.jsx
@@ -2,20 +2,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import AppLayout from "../layout/AppLayout"
 import SingleFilm from "../pages/SingleFilm"
 import Home from "../pages/Home"
-
+import GlobalContext from "../contexts/GlobalContext"
 function App() {
-
+  const [loading, setLoading] = useState(false)
+  const values = {
+    loading,
+    setLoading
+  }
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route index element={<Home />} />
-            <Route path="movies/:id" element={<SingleFilm />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <GlobalContext.Provider value={values}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<AppLayout />}>
+              <Route index element={<Home />} />
+              <Route path="movies/:id" element={<SingleFilm />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </GlobalContext.Provider>
     </>
   )
 }
